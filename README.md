35427d06a6e1249a07c65c8fd095f8b84dec694a
illusionTBA <illusionztba@gmail.com>
2022-04-25 00:07:26 -0400

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Calco-Proxy
+**The proxy of the future, who'll guess it's a proxy?**
+Join our discord [here](https://discord.gg/udNPgGXjws)
+## Credits
+<strong>illusions#3875 - html,js,css (all of the frontend)</strong><br>
+<strong>pyro#0009 - Helped with AB cloak</strong><br>
+<strong>ionetty#3118 - Networking / Backend (all of the backend)</strong><br>
+<strong>JimDude#7404 - The reason can host sites ! (gave us his vps!)</strong><br>
+<strong>Maxysk#5479 - Made the bot in the discord</strong><br>
+
+## Features
+
+| Features      | status       |
+| ------------- |:-------------|
+| Clean-Code    | `🔴`         |
+| front-end     | `🟡`         |
+| Web-proxy     | `🟢`         |
+
+## DO NOT SKID OUR SITE pwease!!!
+
+# Deploy
+[![Deploy to Heroku](https://raw.githubusercontent.com/BinBashBanana/deploy-buttons/master/buttons/remade/heroku.svg)](https://heroku.com/deploy/?template=https://github.com/Calco-Proxy/Calco-Proxy-Public)
+[![Run on Replit](https://raw.githubusercontent.com/BinBashBanana/deploy-buttons/master/buttons/remade/replit.svg)](https://replit.com/github/Calco-Proxy/Calco-Proxy-Public)
+[![Remix on Glitch](https://raw.githubusercontent.com/BinBashBanana/deploy-buttons/master/buttons/remade/glitch.svg)](https://glitch.com/edit/#!/import/github/Calco-Proxy/Calco-Proxy-Public)
---
+++ static/index.js
@@ -0,0 +1,60 @@
+const form = document.querySelector('form');
+const input = document.querySelector('.special-input');
+const iframe = document.querySelector('iframe');
+const searchBTN = document.querySelector('.searchbtn');
+
+
+searchBTN.addEventListener('click', async event => {
+    console.log('clicked');
+    if (localStorage.getItem('AB-cloak-check') === 'true') {
+        console.log('AB-cloak-check is true');
+        event.preventDefault();
+        window.navigator.serviceWorker.register('./sw.js', {
+            scope: __uv$config.prefix
+        }).then(() => {
+            let url = input.value.trim();
+            if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+            else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+            var urle = "http://" + document.domain + __uv$config.prefix + __uv$config.encodeUrl(url);
+            if (urle) {
+                var win; {
+                    if (win) { win.focus(); } else {
+                        win = window.open();
+                        win.document.body.style.margin = '0';
+                        win.document.body.style.height = '100vh';
+                        var iframe = win.document.createElement('iframe');
+                        iframe.style.border = 'none';
+                        iframe.style.width = '100%';
+                        iframe.style.height = '100%';
+                        iframe.style.margin = '0';
+                        iframe.src = urle;
+                        win.document.body.appendChild(iframe)
+                    }
+                }
+            }
+
+        });
+
+    } else {
+        console.log('AB-cloak-check is false');
+        event.preventDefault();
+        window.navigator.serviceWorker.register('./sw.js', {
+            scope: __uv$config.prefix
+        }).then(() => {
+            let url = input.value.trim();
+            if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+            else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+            iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+            document.querySelector(".Logcontaner2").style.display = "block";
+            document.querySelector(".iframeControlls").style.display = "block";
+        });
+    }
+});
+
+function isUrl(val = '') {
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
+
+
